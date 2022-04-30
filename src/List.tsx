5f53efac4d2b1739723aa047516b83bbe2def114
Luan Almeida <luanlmd@gmail.com>
2022-04-30 13:55:11 -0300

    getting some data to work with

--- src/List.tsx
+++ src/List.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import ListItem from "./ListItem";
 
 interface IProps {
@@ -6,11 +7,34 @@ interface IProps {
 
 const List: React.FC<IProps> = (props: IProps) => {
     const items = props.items;
+    const itemCount = props.items.length;
+    const listRef = useRef(null);
 
+    const [scrollTop, setScrollTop] = useState(0);
+    const [listHeight, setListHeight] = useState(0);
+
+    useEffect(() => {
+        if (!listRef.current) { return; } 
+        setListHeight((listRef.current as HTMLElement).clientHeight);
+    },[listRef]);
+
+    const handleScroll = (e: any) => {
+        setScrollTop((e.target as HTMLElement).scrollTop);
+    }
+    
     return (<>
-    <div>
-        { items.map((item, index) => <ListItem key={index} item={item} index={index}></ListItem> ) }
-    </div>
+            <div style={{ position:'absolute', background:'gray', color:'white', right:0, bottom:0, width:'180px' }}>
+                <p>Items: {itemCount}</p>
+                <p>Item Height: </p>
+                <p>List Height: {listHeight}</p>
+                <p>Scroll Pos: {scrollTop}</p> 
+            </div>
+
+            <div onScroll={handleScroll} style={ {display:'block', height:'500px', width:'100%', overflowY:'scroll', background:'red'} }>
+                <div ref={listRef}>
+                    { items.map((item, index) => <ListItem key={index} item={item} index={index}></ListItem> ) }
+                </div>
+            </div>
     </>)
 }
 
--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -6,8 +6,8 @@ interface IProps {
 const ListItem:React.FC<IProps> = (props: IProps) => {
     const item = props.item;
     return (
-        <div>
-            <img src={ item.thumbnailUrl } />
+        <div >
+            <img src={ item.thumbnailUrl } alt={item.title} />
             <div>{ item.id }</div>
             <div>{ item.title }</div>
             <div>{ item.url }</div>
